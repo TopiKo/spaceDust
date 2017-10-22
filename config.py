9e2a612e8d113560989afd5ffde2f91df4ddd513
TopiKo <topiko1987@gmail.com>
2017-10-22 21:27:06 +0200

    Behold the first version of gravity simulation...

---
+++ config.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+r = np.array([[0.,0.], [1.,0.], [0.,1.], [1.,1.]])
+v = np.zeros((len(r), 2))
+a = np.zeros((len(r), 2))
+masses_ = np.ones(len(r))*2
+
+r_ = np.ma.array(r, mask=False)
+v_ = np.ma.array(v, mask=False)
+a_ = np.ma.array(a, mask=False)
+F_ = np.zeros(len(r_))
+dir_ij_arr_ = np.ma.zeros((len(r), len(r), len(r[0])))
