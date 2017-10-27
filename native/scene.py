52c2a95af831ae131061fac838676ac728312055
0mar <omsrichardson@gmail.com>
2017-10-27 18:08:21 +0200

    Removed for loop and some unnessecary calculations

--- native/scene.py
+++ native/scene.py
@@ -5,7 +5,7 @@ from native.geometry import Point, Size
 
 class Scene:
     """
-    Models a scene. A scene is a rectangular object particles inside.
+    Models a scene. A scene is a rectangular object with particles inside.
     """
 
     def __init__(self, num_particles):
@@ -16,18 +16,19 @@ class Scene:
         self.time = 0
         self.dt = 0.005
         self.counter = 0
-        self.size = Size([100,100])
+        self.size = Size([100, 100])
         self.num_particles = num_particles
 
         # Array initialization
-        self.position_array = np.ma.array(np.zeros([self.num_particles,2]),mask=False)
+        self.position_array = np.zeros([self.num_particles, 2])
         self.velocity_array = np.zeros([self.num_particles, 2])
         self.accel_array = np.zeros([self.num_particles, 2])
         self.force_array = np.zeros([self.num_particles, 2])
         self.particle_size = np.zeros([self.num_particles, 2])
         self.masses = np.ones(self.num_particles)
 
-        self.pos_difference = np.ma.zeros((self.num_particles, self.num_particles, 2))
+        self.pos_difference = np.zeros((self.num_particles, self.num_particles, 2))
+        self.mask = 4
         # Parameters
         self.g = 30
         self._init_particles()
@@ -46,14 +47,15 @@ class Scene:
         rn = np.zeros((self.num_particles, 3))
         zs = np.zeros(rn.shape)
         rn[:, :2] = self.position_array
-        zs[:, 2] = 1./100
+        zs[:, 2] = 1. / 100
         self.velocity_array = np.cross(zs, rn)[:, :2]
 
         self.particle_size[:] = np.random.random(self.position_array.shape) + 1
 
         # Masses
-        for i in range(self.num_particles):
-            self.masses[i] = 1
+        self.tiled_masses = np.tile(self.masses, self.num_particles).reshape((self.num_particles, self.num_particles))
+        np.fill_diagonal(self.tiled_masses, 0)
+        self.tiled_masses = self.tiled_masses[:, :, None]  # Probably not a time saver
 
     def is_within_boundaries(self, coord: Point):
         """
@@ -65,19 +67,16 @@ class Scene:
         return within_boundaries
 
     def update_directions(self):
-        copy_pos = self.position_array.copy()
-        for i in range(self.num_particles):
-            self.position_array.mask[i] = True
-            self.pos_difference[i] = self.position_array - copy_pos[i]
-            self.position_array.mask[i] = False
+        trans_pos = np.tile(self.position_array, self.num_particles).reshape(
+            (self.num_particles, self.num_particles, 2))
+        pos = np.transpose(trans_pos, (1, 0, 2))
+        self.pos_difference[:] = pos - trans_pos
+        self.pos_difference[np.arange(self.num_particles), np.arange(self.num_particles), :] = self.mask
 
     def update_forces(self):
         distances = np.linalg.norm(self.pos_difference, axis=2, keepdims=True)  # *masses
-
-        masses = np.tile(self.masses, self.num_particles).reshape((self.num_particles, self.num_particles))
-
-        planet_contributions = self.pos_difference / distances * masses[:, :, None]
-        self.force_array = np.dot(np.diag(self.masses), (planet_contributions / distances ** 2).sum(axis=1)) * self.g
+        planet_contributions = self.pos_difference * self.tiled_masses / distances ** 3
+        self.accel_array = planet_contributions.sum(axis=1) * self.g
 
     def update_pos_and_velo(self):
         """
@@ -87,7 +86,6 @@ class Scene:
         """
         self.time += self.dt
         self.counter += 1
-        self.accel_array = np.dot(np.diag(1 / self.masses), self.force_array)
         self.velocity_array += self.accel_array * self.dt
         self.position_array += self.velocity_array * self.dt
         self.position_array = np.ma.array(self.position_array, mask=False)
